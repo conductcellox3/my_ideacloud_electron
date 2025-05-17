ae8dab2965199e1860d33767fa4b5e4faa22bc0f
conconcon <conductcellox3@gmail.com>
2025-05-18 00:49:22 +0900

    initial commit

---
+++ electron/main.ts
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+        },
+    });
+
+    if (isDev) {
+        win.loadURL('http://loaclhost:3000');
+        win.webContents.openDevTools();
+    } else {
+        win.loadFile(path.join(__dirname, '../renderer/index.html'));
+    }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
